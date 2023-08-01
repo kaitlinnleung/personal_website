43c1184e8136abce353313593ddee7925f82dac7
kaitlinnleung <114363505+kaitlinnleung@users.noreply.github.com>
2023-07-31 23:35:10 -0700

    Add files via upload

---
+++ one.js
@@ -0,0 +1,9 @@
+function dropdown() {
+}
+
+
+function copyEmail() {
+    navigator.clipboard.writeText("kaitlinleung@my.smccd.edu");
+    alert( "Copied kaitlinleung@my.smccd.edu to clipboard" );
+    console.log("weee");
+}
