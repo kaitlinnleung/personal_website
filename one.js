553235fde3cb7c01374c14e67274baa548cc83b4
kaitlinnleung <114363505+kaitlinnleung@users.noreply.github.com>
2023-08-01 19:26:04 -0700

    Delete one.js

--- one.js
+++
@@ -1,9 +0,0 @@
-function dropdown() {
-}
-
-
-function copyEmail() {
-    navigator.clipboard.writeText("kaitlinleung@my.smccd.edu");
-    alert( "Copied kaitlinleung@my.smccd.edu to clipboard" );
-    console.log("weee");
-}
