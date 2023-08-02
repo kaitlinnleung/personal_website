02b5a51547523c23aa1a5077030aec9037b7d1b3
kaitlinnleung <114363505+kaitlinnleung@users.noreply.github.com>
2023-08-01 19:19:03 -0700

    submitting final

---
+++ personal_website_final/one.js
@@ -0,0 +1,9 @@
+function copyEmail() {
+    navigator.clipboard.writeText("kaitlinleung@my.smccd.edu");
+    alert( "Copied kaitlinleung@my.smccd.edu to clipboard" );
+}
+
+function copyPhone() {
+    navigator.clipboard.writeText("6506697121");
+    alert( "Copied 6506697121 to clipboard" );
+}
